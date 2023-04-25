700b9a353024da6857c437a51e8a8e66d7063e46
Joao Barbara <jvictor32@gmail.com>
2023-04-25 11:10:55 -0300

    fix url path and catch exceptions

--- extrator_links.py
+++ extrator_links.py
@@ -1,36 +1,40 @@
+#!/usr/bin/env python
+
 import requests
-from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
-
-import urllib.request, urllib.error, urllib.parse
-
+import os
 import validators
+from bs4 import BeautifulSoup
+from urllib import (request, error, parse)
+from urllib.request import (Request, urlopen)
 
-num_niveis = 2
-
-headers = {'Content-Type': 'text/html', 'User-Agent': 'Mozilla/5.0'}
+HEADERS = {'Content-Type': 'text/html', 'User-Agent': 'Mozilla/5.0'}
+OUTPUT_FILE = './downloaded_pages/'
+LOGERROR = './log.error'
 
 def download_webpage(url, num):
+    try:
+        req = Request(
+            url=url, 
+            headers=HEADERS
+        )
+        response = urlopen(req)
 
-    req = Request(
-        url=url, 
-        headers=headers
-    )
-    response = urlopen(req)
+        #response = urllib.request.urlopen(url)
+        webContent = response.read().decode(response.headers.get_content_charset())#.decode('UTF-8')
 
-    #response = urllib.request.urlopen(url)
-    webContent = response.read().decode(response.headers.get_content_charset())#.decode('UTF-8')
+        f = open(os.path.join(OUTPUT_FILE, str(num)+'.txt'), 'w', encoding='UTF-8')
+        f.write(webContent)
+        f.close
+    except Exception:
+        with open(LOGERROR, 'a') as log_error:
+            log_error.write(url + '\n')
 
-    f = open(str(num)+'.txt', 'w', encoding='UTF-8')
-    f.write(webContent)
-    f.close
 
 def get_links(url):
 
     num = 0
-
     links = []
-    website = requests.get(url, headers=headers)
+    website = requests.get(url, headers=HEADERS)
     website_text = website.text
 
     soup = BeautifulSoup(website_text, 'html.parser')
@@ -40,9 +44,11 @@ def get_links(url):
 
     for link in links:
 
-        if(str(link) != 'None'):
-            if validators.url(link):
-                download_webpage(link, num)
+        if (str(link) != 'None'):
+            if validators.url(url + '/' + link):
+                download_webpage(url + '/' + link, num)
                 num += 1
 
-get_links('https://opendatasus.saude.gov.br')
+
+if __name__ == '__main__':
+    get_links('https://opendatasus.saude.gov.br')
